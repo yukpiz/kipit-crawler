1228b1c9a42293bbfd15d36d2cfb752ee2c8e90c
yukpiz <yukpiz@gmail.com>
2016-10-25 16:09:01 +0900

    Updated.

--- core/twitter/twitter.rb
+++ core/twitter/twitter.rb
@@ -1,8 +1,12 @@
 require "twitter"
+require "tweetstream"
 require "./core/config/config.rb"
 
 class TwitterClient
+  attr_accessor :stream
+
   @client = nil
+  @stream = nil
   @conf = nil
   def initialize
     @conf = Config.load
@@ -12,24 +16,28 @@ class TwitterClient
       config.access_token = ENV[@conf['twitter']['access_token']]
       config.access_token_secret = ENV[@conf['twitter']['access_token_secret']]
     end
+
+    TweetStream.configure do |config|
+      config.consumer_key = ENV[@conf['twitter']['consumer_key']]
+      config.consumer_secret = ENV[@conf['twitter']['consumer_secret']]
+      config.oauth_token = ENV[@conf['twitter']['access_token']]
+      config.oauth_token_secret = ENV[@conf['twitter']['access_token_secret']]
+      config.auth_method = :oauth
+    end
+    @stream = TweetStream::Client.new
   end
 
-  def get_followers
-    @client.followers(@conf['twitter']['screen_name'])
+  def get_follower_ids
+    @client.follower_ids(@conf['twitter']['screen_name']).to_a
   end
 
-  def get_follows
-    @client.friends(@conf['twitter']['screen_name'])
+  def get_follow_ids
+    @client.follow_ids(@conf['twitter']['screen_name']).to_a
   end
 
   def refollow
-    followers = get_followers
-    follows = []
-    followers.each do |follower|
-      follow = @client.follow(follower.id)
-      follows.push(follow.shift) if follow.count > 0
-    end
-    return follows
+    follower_ids = get_follower_ids
+    @client.follow(follower_ids)
   end
 
   def send_text(text)
--- kipit-twitter.rb
+++ kipit-twitter.rb
@@ -1,33 +1,39 @@
 require "./core/twitter/twitter.rb"
 require "./core/logger/log.rb"
+require "./core/config/config.rb"
 
 threads = []
 #Initialize logger
 log = Log.new
+#Initialize configuration
+conf = Config.load
+#Initialize Twitter client
+twitter = TwitterClient.new
 
 #Twitter refollow observer thread
 threads.push(Thread.new do
-  twitter = TwitterClient.new
   while (true)
     sleep 60
-    follows = twitter.refollow
-    follows.each do |follow|
-      sleep 10
-      reply_text = <<"EOS"
-@#{follow.screen_name}
-はじめましてっ！
-ムーニャと言います(๑•̀ㅂ•́)و✧
-helpでリプライすると使い方がわかるよっ(•ө•)♡
-EOS
+    new_follows = twitter.refollow
+    next if new_follows.empty?
+
+    reply_text_template = conf['twitter']['texts']['refollow_text']
+    new_follows.each do |new_follow|
+      log.info("#{new_follow.screen_name} 新しいユーザーをフォローしました。")
+      reply_text = "@#{new_follow.screen_name} #{reply_text_template}"
       twitter.send_text(reply_text)
-      sleep 10
     end
   end
 end)
 
 #Twitter reply ebserver thread
 threads.push(Thread.new do
-  twitter = TwitterClient.new
+  twitter.stream.userstream do |status|
+    next if not status.text =~ /^@#{conf['twitter']['screen_name']}\s*/
+    if status.text =~ /\+.\s*/
+      puts status.text.match(/\+ (.+)/)
+    end
+  end
 end)
 
 begin
